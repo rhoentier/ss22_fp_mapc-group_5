e9bef5647d35a6c802e2b04cc22192ad930475f7
rhoentier <sjendrny@posteo.de>
2022-04-27 11:57:28 +0200

    fix conf

--- StartServerAndAgent.ps1
+++ StartServerAndAgent.ps1
@@ -4,6 +4,6 @@ Start-Process -FilePath java -ArgumentList '-jar .\massim_2022\server\target\ser
 Start-Sleep -s 1
 
 Write-Host "Starting Agents"
-Start-Process -FilePath java -ArgumentList '-jar .\massim_2022\javaagents\target\javaagents-2022-1.0-jar-with-dependencies.jar -conf .\massim_2022\javaagents\conf\BasicAgents\'
+Start-Process -FilePath java -ArgumentList '-jar .\massim_2022\javaagents\target\javaagents-2022-1.0-jar-with-dependencies.jar -conf .\massim_2022\javaagents\conf\NextAgents\'
 
 Exit
