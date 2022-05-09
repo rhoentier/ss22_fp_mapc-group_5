4144ca03357c5a5aa68264d9c2b325567716f955
rhoentier <sjendrny@posteo.de>
2022-05-09 18:00:47 +0200

    add methods

--- massim_2022/javaagents/src/main/java/massim/javaagents/agents/TimeMonitor.java
+++ massim_2022/javaagents/src/main/java/massim/javaagents/agents/TimeMonitor.java
@@ -1,35 +1,58 @@
 package massim.javaagents.agents;
 
+import java.time.Instant;
+
 public class TimeMonitor {
     private long simulationStartTime;
     private long roundStartTime;
     private long roundDeadline;
-
-    // Ein Offset, damit am Ende noch Zeit für den Versand über bleibt
+    /**
+     * Offeset, damit genügend Zeit zur Kommunikation mit dem Server bleibt
+     */
     private long secureOffset = 100;
 
     public TimeMonitor(long simulationStartTime) {
         this.simulationStartTime = simulationStartTime;
     }
 
+    /**
+     * Setzt zu beginn die Start und Deadline-Werte. Ermittelt außerdem ein SecureOffset,
+     * das sich aus der Zeit der Kommunikation ermittelt
+    */
     public void SetRoundTime(long roundStartTime, long roundDeadline){
+        long long currentTime = Instant.now().toEpochMilli();
         this.roundStartTime = roundStartTime;
         this.roundDeadline = roundDeadline;
+        this.secureOffset = roundStartTime - currentTime;
     }
 
+    /**
+     * SecureOffset kann bei Kommunikationsproblemen individuell gesetzt werden
+     * @param secureOffset
+     */
     public void SetSecureOffset(long secureOffset) {
         this.secureOffset = secureOffset;
     }
 
-    // Gibt zurück, ob noch Zeit für eine Berechnung bleibt
+    /**
+     * Gibt zurück, ob noch Zeit bis zur Deadline verfügbar ist
+     * @return
+     */
     public boolean IsTimeRemaining(){
-        // TODO: Prüfen, ob noch Zeit ist um Berechnung auszufüren
-        return True;
+        long currentTime = Instant.now().toEpochMilli();
+        if ((currentTime + secureOffset) < roundDeadline) {
+            return true;
+        }
+        return false;
     }
 
-    // Gibt die restliche Zeit bis zum Rundenende an
+    /**
+     * Gibt die restliche Zeit, abzüglich des SecureOffset an
+     * @return restliche Zeit im Millisekunden
+     */
     public long GetRemainingTime(){
-        // TODO: Restliche Zeit berechnen
-        return 0;
+        if(!IsTimeRemaining()) return 0;
+        long currentTime = Instant.now().toEpochMilli();
+        return roundDeadline - (currentTime + secureOffset)
     }
 }
