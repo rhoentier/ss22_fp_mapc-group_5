c7bf55d7f5c5684b92182b3a42b8f92d4eb0f650
seblod90 <sebastian.loder@icloud.com>
2022-05-15 12:36:33 +0200

    Zusammenführen mehrerer Karten, Speichern von Positionen von Point auf Vector2D umgestellt

--- massim_2022/javaagents/src/main/java/massim/javaagents/agents/Map.java
+++ massim_2022/javaagents/src/main/java/massim/javaagents/agents/Map.java
@@ -1,14 +1,16 @@
-import java.awt.*;
+package massim.javaagents.agents;
+import java.util.Objects;
 
 public class Map {
 
     private MapTile[][] map;
-    private Point zeroPoint;
+    private Vector2D zeroPoint;
+    public Boolean foundDispenser = false;
 
     public Map() {
         map = new MapTile[20][20];
-        zeroPoint = new Point(0, 0);
-        // ToDo: If implementation should be more efficient: Either add flags or separate list for static things.
+        zeroPoint = new Vector2D(0, 0);
+        // ToDo: If implementation should be more efficient: Store separate list for static things instead of flags.
     }
 
     /**
@@ -16,14 +18,13 @@ public class Map {
      * @param agentPosition Current position of the agent relative to the starting position.
      * @param percept Array of things as MapTile-objects.
      */
-    public void AddPercept(Point agentPosition, MapTile[] percept) {
+    public void AddPercept(Vector2D agentPosition, MapTile[] percept) {
 
-        Point mapTilePosition = new Point();
+        Vector2D mapTilePosition = new Vector2D();
         for (MapTile mapTile : percept) {
-            mapTilePosition.setLocation(agentPosition);
-            mapTilePosition.translate(mapTile.getPositionX(), mapTile.getPositionY());
-            extendArray(mapTilePosition);
-            addMapTile(mapTilePosition, mapTile);
+            mapTilePosition.set(agentPosition);
+            mapTilePosition.add(mapTile.getPositionX(), mapTile.getPositionY());
+            setMapTileRel(mapTilePosition, mapTile);
         }
     }
 
@@ -48,90 +49,196 @@ public class Map {
     }
 
     /**
-     * Adds a MapTile-object to the map.
-     * @param position: Position of the map tile relative to the starting position of the agent.
-     * @param maptile: MapTile to add.
+     * Merges the given map (param 1) into the existing map (this) based on position of agents and in which distance
+     * they see each other.
+     * @param mapAgent2 Map of the agent2, which is seen from agent1.
+     * @param positionAgent1 Position of agent1, which sees agent2
+     * @param positionAgent2 Position of agent2, which is seen from agent1.
+     * @param deltaView Distance in which agent1 sees agent2
      */
-    private void addMapTile(Point position, MapTile maptile) {
-        this.map[this.zeroPoint.x + position.x][this.zeroPoint.y + position.y] = maptile;
+    public void mergeMap(Map mapAgent2, Vector2D positionAgent1, Vector2D positionAgent2, Vector2D deltaView) {
+
+        // Calculate vector from starting point of agent1 (on map1) to absolute zero on map2
+        Vector2D displacementVector = new Vector2D(positionAgent1);
+        displacementVector.add(deltaView);
+        displacementVector.subtract(positionAgent2);
+        displacementVector.subtract(mapAgent2.zeroPoint);
+
+        for (int i = 0; i < mapAgent2.getSizeOfMap().x; i++) {
+            for (int j = 0; j < mapAgent2.getSizeOfMap().y; j++) {
+                Vector2D insertAt = new Vector2D(displacementVector);
+                insertAt.add(i, j);
+                setMapTileRel(insertAt, mapAgent2.map[i][j]);
+            }
+        }
     }
 
     /**
-     * Removes a MapTile-object from the map.
-     * @param position: Position of the map tile relative to the starting position of the agent.
+     * Finds the relative position of a map tile with a specific thing type
+     * @param thingType Thing type to be found, e.g. "dispenser"
      */
-    private void removeMapTile(Point position) {
-        this.map[this.zeroPoint.x + position.x][this.zeroPoint.y + position.y] = null;
+    public Vector2D getNearest(String thingType, Vector2D agentPosition) {
+        double shortestDistanceSoFar = Double.POSITIVE_INFINITY;
+        double currentDistance;
+        Vector2D absolutePosition = null;
+        Vector2D agentPositionAbs = relativeToAbsolute(agentPosition);
+
+        for (int i = 0; i < this.map.length; i++) {
+            for (int j = 0; j < this.map[i].length; j++) {
+                if (Objects.equals(getMapTileAbs(new Vector2D(i, j)).getThingType(), thingType)) {
+                    currentDistance = agentPositionAbs.distance(new Vector2D(i, j));
+                    if (currentDistance < shortestDistanceSoFar) {
+                        shortestDistanceSoFar = currentDistance;
+                        absolutePosition = new Vector2D(i, j);
+                    }
+                }
+            }
+        }
+        return absoluteToRelative(absolutePosition);
+    }
+
+    /**
+     * Transforms a relative position to an absolute position. Example with grid 10/10 and zero point at 5/5.
+     * Coordinate 1/1 (relative) is transformed to 6/6 (absolute).
+     * @param relativeVector Relative position on the map
+     * @return Absolute position on the map
+     */
+    private Vector2D relativeToAbsolute(Vector2D relativeVector) {
+        return new Vector2D(relativeVector).getAdded(zeroPoint);
+    }
+
+    /**
+     * Transforms an absolute position to a relative position. Example with grid 10/10 and zero point at 5/5.
+     * Coordinate 1/1 (absolute) is transformed to -4/-4 (relative).
+     * @param absoluteVector Absolute position on the map
+     * @return Relative position on the map
+     */
+    private Vector2D absoluteToRelative(Vector2D absoluteVector) {
+        return new Vector2D(absoluteVector).getSubtracted(zeroPoint);
+    }
+
+    /**
+     * Returns a map tile at an absolute position
+     * @param absolutePosition
+     * @return maptile object
+     */
+    private MapTile getMapTileAbs(Vector2D absolutePosition) {
+        return map[(int)absolutePosition.x][(int)absolutePosition.y];
+    }
+
+    /**
+     * Returns a map tile at a relative position
+     * @param relativePosition
+     * @return maptile object
+     */
+    private MapTile getMapTileRel(Vector2D relativePosition) {
+        Vector2D absolutePosition = relativeToAbsolute(relativePosition);
+        return getMapTileAbs(absolutePosition);
+    }
+
+    /**
+     * Sets an object on the position relative to the starting position of the agent.
+     * @param relativePosition: Position of the map tile relative to the starting position of the agent.
+     * @param maptile: MapTile to add.
+     */
+    private void setMapTileRel(Vector2D relativePosition, MapTile maptile) {
+        Vector2D absPosition = relativeToAbsolute(relativePosition);
+        setMapTileAbs(absPosition, maptile);
+    }
+
+    /**
+     * Sets an object on the absolute position of the map.
+     * @param absolutePosition: Position of the map tile absolute from the top left point.
+     * @param maptile: MapTile to add.
+     */
+    private void setMapTileAbs(Vector2D absolutePosition, MapTile maptile) {
+
+        MapTile existingMapTile;
+
+        // Set flags for some thing types. Flags can be checked before accessing the array.
+        if (maptile != null) {
+            switch (maptile.getThingType()) {
+                case "dispenser":
+                    foundDispenser = true;
+                    // ToDo: Can be extended with other static "things" which should be stored as flags.
+            }
+        }
+
+        Vector2D offset = new Vector2D(extendArray(absolutePosition));
+        absolutePosition.add(offset);
+
+        existingMapTile = this.map[(int)absolutePosition.x][(int)absolutePosition.y];
+
+        if (existingMapTile == null || existingMapTile.getLastVisionStep() <= existingMapTile.getLastVisionStep()) {
+            this.map[(int)absolutePosition.x][(int)absolutePosition.y] = maptile;
+        }
     }
 
     /**
      * Calculates the most positive coordinate possible for the current map. If the map is of size 10/10 and the
      * zero point is at 5/5, the most positive coordinate is at 4/4.
-     * @return Point with most positive coordinate
+     * @return Vector with most positive coordinate
      */
-    private Point getPositiveExtend() {
-        return new Point (map.length-zeroPoint.x-1, map[0].length-zeroPoint.y-1);
+    private Vector2D getPositiveExtend() {
+        return new Vector2D (map.length-(int)zeroPoint.x-1, map[0].length-(int)zeroPoint.y-1);
     }
 
     /**
      * Calculates the most negative Coordinate possible for the current map. If the map is of size 10/10 and the
      * zero point is at 5/5, the most negative coordinate is -5/-5.
-     * @return Point with most negative coordinate
+     * @return Vector with most negative coordinate
      */
-    private Point getNegativeExtend() {
-        return new Point (-1 * zeroPoint.x, -1 * zeroPoint.y);
+    private Vector2D getNegativeExtend() {
+        return new Vector2D (-1 * (int)zeroPoint.x, -1 * (int)zeroPoint.y);
     }
 
     /**
-     * Returns the size of the map.
-     * @return Point object, which represents the number of elements in x- and y-direction.
+     * Calculates the size of the map.
+     * @return Vector object, which represents the number of elements in x- and y-direction.
      */
-    private Point getSizeOfMap() {
-        return new Point (map.length, map[0].length);
+    private Vector2D getSizeOfMap() {
+        return new Vector2D (map.length, map[0].length);
     }
+
     /**
      * Extends the size of the map object either in x+, x-, y+ or y- direction if the map is too small.
      * @param positionMapTile position of the map tile to be added relative to the starting position.
      */
-    private void extendArray(Point positionMapTile) {
+    private Vector2D extendArray(Vector2D positionMapTile) {
 
-        int offsetX = 0;
-        int offsetY = 0;
         int minExtend = 1;
-        int numExtend;
+        Vector2D numExtend = new Vector2D(0, 0);
+        Vector2D offset = new Vector2D(0, 0);
+        Vector2D sizeOfMap = getSizeOfMap();
 
-        Point maxNegExt = getNegativeExtend();
-        Point maxPosExt = getPositiveExtend();
-        Point sizeOfMap = getSizeOfMap();
-
-        if (positionMapTile.x > maxPosExt.x) {            // x+
-            numExtend = Math.max(positionMapTile.x - maxPosExt.x, minExtend);
-            sizeOfMap.x += numExtend;
-        } else if (positionMapTile.x < maxNegExt.x) {     // x-
-            numExtend = Math.max(maxNegExt.x - positionMapTile.x, minExtend);
-            sizeOfMap.x += numExtend;
-            offsetX = numExtend;
-        } else if (positionMapTile.y > maxPosExt.y) {     // y+
-            numExtend = Math.max(positionMapTile.y - maxPosExt.y, minExtend);
-            sizeOfMap.y += numExtend;
-        } else if (positionMapTile.y < maxNegExt.y) {     // y-
-            numExtend = Math.max(maxNegExt.y - positionMapTile.y, minExtend);
-            sizeOfMap.y += numExtend;
-            offsetY = numExtend;
-        } else {
-            return;
+        if (positionMapTile.x >= map.length) {
+            numExtend.x = Math.max(positionMapTile.x - map.length + 1, minExtend);
+        } else if (positionMapTile.x < 0) {
+            numExtend.x = Math.max(Math.abs(positionMapTile.x), minExtend);
+            offset.x = numExtend.x;
         }
 
-        MapTile[][] tmp = new MapTile[sizeOfMap.x][sizeOfMap.y];
+        if (positionMapTile.y >= map[0].length) {
+            numExtend.y = Math.max(positionMapTile.y - map[0].length + 1, minExtend);
+        } else if (positionMapTile.y < 0) {
+            numExtend.y = Math.max(Math.abs(positionMapTile.y), minExtend);
+            offset.y = numExtend.y;
+        }
 
-        for (int i = 0; i < this.map.length; i++) {
-            for (int j = 0; j < this.map[i].length; j++) {
-                tmp[i + offsetX][j + offsetY] = this.map[i][j];
+        if (numExtend.getLength() > 0) {
+            sizeOfMap.add(numExtend);
+
+            MapTile[][] tmp = new MapTile[(int)sizeOfMap.x][(int)sizeOfMap.y];
+
+            // Copy existing map to tmp map
+            for (int i = 0; i < this.map.length; i++) {
+                for (int j = 0; j < this.map[i].length; j++) {
+                    tmp[i + (int)offset.x][j + (int)offset.y] = this.map[i][j];
+                }
             }
+            this.zeroPoint.add(offset.x, offset.y);
+            this.map = tmp;
         }
-
-        this.zeroPoint.translate(offsetX, offsetY);
-        this.map = tmp;
+        return offset;
     }
-
 }
