1a912d0ea8f93334751bd0d35b1ebd16da860428
rhoentier <sjendrny@posteo.de>
2022-05-16 19:29:29 +0200

    add new Actions to Action Wrapper

--- massim_2022/javaagents/src/main/java/massim/javaagents/general/NextActionWrapper.java
+++ massim_2022/javaagents/src/main/java/massim/javaagents/general/NextActionWrapper.java
@@ -2,6 +2,7 @@ package massim.javaagents.general;
 
 import eis.iilang.Action;
 import eis.iilang.Identifier;
+import eis.iilang.Parameter;
 
 public class NextActionWrapper {
 
@@ -13,25 +14,57 @@ public class NextActionWrapper {
      * @param parameter
      * @return An Action with parameter or null if a required parameter is missing
      */
-    public static Action createAction(NextConstants.EActions action, String parameter) {
+    public static Action createAction(NextConstants.EActions action, Identifier parameter) {
         switch (action) {
+            case attach: {
+                if (parameter != null) {
+                    return new Action("attach", parameter);
+                }
+                return null;
+            }
             case rotate: {
                 if (parameter != null) {
-                    if (parameter == "ccw" || parameter == "cw") {
-                        return new Action("rotate", new Identifier(parameter));
+                    if (parameter.getValue() == "ccw" || parameter.getValue() == "cw") {
+                        return new Action("rotate", parameter);
                     }
                 }
                 return null;
             }
-            case adopt: {
+            case request: {
                 if (parameter != null) {
-                    return new Action("adopt", new Identifier(parameter));
+                    return new Action("request", parameter);
                 }
                 return null;
+            }
+            case adopt: {
+                if (parameter != null)
+                    return new Action("adopt", parameter);
+
+                return null;
+            }
+            default:
+                if (parameter == null) return createAction(action);
+                return null;
+        }
+
+    }
+
+    /**
+     * Create an Action with no Parameters.
+     *
+     * @param action
+     * @return An Action without a parameter
+     */
+    public static Action createAction(NextConstants.EActions action) {
+        switch (action) {
+            case skip: {
+                return new Action("skip");
             }
             default:
                 return null;
         }
 
     }
+
 }
+
