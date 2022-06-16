f5eb9d091cedcb84fe80c904a3e7503eb57a2dd8
rhoentier <sjendrny@posteo.de>
2022-06-16 20:11:18 +0200

    add tastPlanner

---
+++ massim_2022/javaagents/src/main/java/massim/javaagents/tasks/NextPlan.java
@@ -0,0 +1,13 @@
+package massim.javaagents.tasks;
+
+import massim.javaagents.general.NextConstants;
+
+import java.util.ArrayList;
+
+public class NextPlan {
+
+    private NextConstants.
+    private ArrayList<NextPlan> subPlans = new ArrayList<NextPlan>();
+    private boolean isPreconditionFulfilled = false;
+
+}
---
+++ massim_2022/javaagents/src/main/java/massim/javaagents/tasks/NextTaskPlanner.java
@@ -0,0 +1,11 @@
+package massim.javaagents.tasks;
+
+import massim.javaagents.percept.NextTask;
+
+import java.util.ArrayList;
+
+public class NextTaskPlanner {
+
+    private ArrayList<NextTask> currentTasks = new ArrayList<NextTask>();
+    private NextPlan currentPlan = new NextPlan();
+}
